d7dccd224fd8046f319683a5776507bdea28f6d2
Toms <toms@buu.lv>
2019-01-08 17:49:24 +0200

    Undo

--- blacklist-named.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-LISTS="ad.sites phishing.sites pushing.sites yoyo.ad.sites"
-
-for LIST in $LISTS
-do
-  curl "https://git.buu.lv:10080/tom/blacklist-named/raw/master/$LIST.conf" | sed 's/null.zone.file/\/etc\/bind\/null.zone.file/g' > /etc/bind/$LIST.conf
-done
