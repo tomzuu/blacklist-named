d61b8aa40046d7dfa88b858c4f440cfcd9e9ca4a
Toms <toms@buu.lv>
2019-10-08 12:33:56 +0300

    added wget eq if curl is not available

--- blacklist-named.sh
+++ blacklist-named.sh
@@ -5,4 +5,6 @@ LISTS="ad.sites phishing.sites pushing.sites yoyo.ad.sites"
 for LIST in $LISTS
 do
   curl -L "https://github.com/tomzuu/blacklist-named/raw/master/$LIST.conf" | sed 's/null.zone.file/\/etc\/bind\/null.zone.file/g' > /etc/bind/$LIST.conf
+
+  #wget -qO- "https://github.com/tomzuu/blacklist-named/raw/master/$LIST.conf" | sed 's/null.zone.file/\/etc\/bind\/null.zone.file/g' > /etc/bind/$LIST.conf
 done
